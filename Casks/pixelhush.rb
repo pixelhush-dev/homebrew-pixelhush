1285ce730a47d0434ee42e882b35da1f0bf598cc
Даниил Постнов <dan.postnov@gmail.com>
2026-03-19 08:43:53 +0500

    chore: update pixelhush 1.3.1

--- Casks/pixelhush.rb
+++ Casks/pixelhush.rb
@@ -1,6 +1,6 @@
 cask "pixelhush" do
   version "1.3.1"
-  sha256 "ed2850493ff13d97fa97bb5fa0aa27e7ae6a480826ce99a8fc2e70df0357fe5b"
+  sha256 "a7c05f26377c9bf273779feb10e580948165f815066dcde35e800dd94fcf885e"
 
   url "https://pixelhush.dev/releases/PixelHush-#{version}.dmg"
   name "PixelHush"
