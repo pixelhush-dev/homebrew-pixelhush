80063b33967926447dbbebc8b59296c901df2687
Даниил Постнов <dan.postnov@gmail.com>
2026-03-22 19:10:46 +0500

    chore: update pixelhush 1.5.0

--- Casks/pixelhush.rb
+++ Casks/pixelhush.rb
@@ -1,6 +1,6 @@
 cask "pixelhush" do
-  version "1.3.3"
-  sha256 "bb810a92bdd68eef0b5d07ba0df373f63fb5ff365a3c180655535278d254f24a"
+  version "1.5.0"
+  sha256 "67212cc5d5e28930bb8c4af4e0cdbe4f7082c879f92fe88bb8171879a047b95d"
 
   url "https://pixelhush.dev/releases/PixelHush-#{version}.dmg"
   name "PixelHush"
@@ -15,7 +15,7 @@ cask "pixelhush" do
                    sudo: false
   end
 
-  uninstall quit: "dev.pixelhush.agent"
+  uninstall quit: "com.daniilpostnov.pixelhush"
 
   zap trash: "~/.pixelhush"
 end
